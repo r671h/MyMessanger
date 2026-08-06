787566b8a4db80634d5483306e0ae7acd7d9bdfb
Ruslan <sergeikashtan1337@gmail.com>
2026-08-06 19:31:37 +0200

    Migrate all uploads to Cloudinary

--- client/src/components/VideoPlayer.tsx
+++ client/src/components/VideoPlayer.tsx
@@ -8,49 +8,14 @@ interface VideoPlayerProps {
 
 export function VideoPlayer({src,fileName} : VideoPlayerProps) {
     const videoRef = useRef<HTMLVideoElement>(null);
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [isMuted,setIsMuted] = useState(false);
 
-    const togglePlay = () => {
-        const video = videoRef.current;
 
-        if(!video) return;
-        if(video.paused) {
-            video.play();
-            setIsPlaying(true);
-        }   
-        else {
-            video.pause();
-            setIsPlaying(false);
-        }
-    };
-
-    const toggleMute = () => {
-        const video = videoRef.current;
-        if(!video) return;
-
-        if(video.muted) {
-            video.muted = false;
-            setIsMuted(false);
-        }
-        else {
-
-        }
-    }
-
-    const toggleFullscreen = () => {
-        videoRef.current?.requestFullscreen();
-    };
-    
     return (
         <div className="relative max-w-md rounded-lg overflow-hidden bg-black group">
         <video
             ref={videoRef}
             src={src}
             className="w-full max-h-96"
-            onClick={togglePlay}
-            onPlay={() => setIsPlaying(true)}
-            onPause={() => setIsPlaying(false)}
             controls
         />
         {fileName && (
